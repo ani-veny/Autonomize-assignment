1de74a4e17ff0c63c449b9a528f6d41ae57997ef
aniveny <aniketald2018@gmail.com>
2024-01-24 18:57:48 +0530

    User route updated

--- routers/user.route.js
+++ routers/user.route.js
@@ -6,7 +6,7 @@ const router=express.Router();
 router.post('/:username', userController.addUser);
 router.get('/', userController.getUsers);
 router.get('/search', userController.getuserBylocationOrUserName)
-router.delete('/:userName', userController.getUserByUserName);
-router.patch('/:userName', userController.getUserByUserName);
+router.delete('/:userName', userController.updateUser);
+router.patch('/:userName', userController.deleteUser);
 
 module.exports= router;
